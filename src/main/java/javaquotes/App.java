b906cbb12978bee927069def032808b55ed8aaa4
Kishor Pandey <kishor.jpandey@gmail.com>
2019-06-09 16:02:43 -0700

    added the tests

--- src/main/java/javaquotes/App.java
+++ src/main/java/javaquotes/App.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 public class App {
     public static void main (String[] args) {
         readFile();
+
     }
 
 
@@ -34,6 +35,7 @@ public class App {
         }
 
     }
+
     public static int getRandom(int num){
         return (int)Math.floor(Math.random()*(num));
     }
